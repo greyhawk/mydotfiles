d5881431e819c17f18e3622fa4b55cf14db1da7b
Ging <wu.tongqing@gmail.com>
2016-08-30 08:56:38 +0800

    modify init.el

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -1,7 +1,7 @@
 function fish_greeting
 end
 
-set -x PATH $PATH ~/.nvm/versions/node/v6.3.1/bin
+set -x PATH $PATH ~/.nvm/versions/node/v6.5.0/bin
 function nvm
     bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
 end
