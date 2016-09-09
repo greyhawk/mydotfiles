6529093882e06ba0d987a545c66b52fe1383d0a9
Ging <wu.tongqing@gmail.com>
2016-09-09 12:35:46 +0800

    add hub and path /usr/local/sbin

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -3,11 +3,11 @@ end
 set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0_102.jdk/Contents/Home/jre
 set -gx ANDROID_HOME /usr/local/opt/android-sdk
 set -gx GROOVY_HOME /usr/local/opt/groovy/libexec
-set -gx RANCHER_URL https://rancher.conloseo.io
-set -gx RANCHER_ACCESS_KEY 438216543CDA90DFF28A
-set -gx RANCHER_SECRET_KEY DCX2PbEvuqqed7c5oZWEXYX1NLCLrD5xV7zHuv4Y
+set -gx RANCHER_URL https://rancher.coloseo.io
+set -gx RANCHER_ACCESS_KEY '438216543CDA90DFF28A'
+set -gx RANCHER_SECRET_KEY 'DCX2PbEvuqqed7c5oZWEXYX1NLCLrD5xV7zHuv4Y'
 
-set -x PATH $PATH ~/.nvm/versions/node/v6.5.0/bin
+set -x PATH $PATH /usr/local/sbin ~/.nvm/versions/node/v6.5.0/bin
 
 function nvm
     bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
