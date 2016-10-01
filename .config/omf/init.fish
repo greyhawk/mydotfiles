dd4ad268031ed7ec60befdf4dabac4532b771ae0
Ging <wu.tongqing@gmail.com>
2016-10-01 23:47:03 +0800

    [Ging.Wu] modify dotfile

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -7,7 +7,7 @@ set -gx RANCHER_URL https://rancher.coloseo.io
 set -gx RANCHER_ACCESS_KEY '438216543CDA90DFF28A'
 set -gx RANCHER_SECRET_KEY 'DCX2PbEvuqqed7c5oZWEXYX1NLCLrD5xV7zHuv4Y'
 set -gx HOMEBREW_GITHUB_API_TOKEN 'f6523f99cbd4d7fa10bed8d6e314c523bca0a89e'
-set -x PATH $PATH /usr/local/sbin ~/.nvm/versions/node/v6.6.0/bin
+set -x PATH $PATH /usr/local/sbin ~/.nvm/versions/node/v6.7.0/bin
 
 function nvm
     bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
--- init.el
+++ init.el
@@ -1,4 +1,11 @@
 ;; 显示行号
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (global-linum-mode 0)
 ;; 自动备份
 (setq backup-directory-alist `(("." . "~/.saves")))
@@ -41,3 +48,17 @@
   '(progn
      (require 'tern-auto-complete)
      (tern-ac-setup))) 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (nginx-mode groovy-mode tern-auto-complete pallet markdown-mode js3-mode evil edts editorconfig dockerfile-mode color-theme-solarized alchemist))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
