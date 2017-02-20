8f698f83ad7bc9a30d80f7db802b0dc506386e4e
Ging.wu <wu.tongqing@gmail.com>
2017-02-20 23:12:39 +0800

    add spacemacs dot file

---
+++ spacemacs/init-archives.el
@@ -0,0 +1,6 @@
+(setq configuration--layer--elpa-archives
+      '(("melpa-cn" . "https://elpa.emacs-china.org/melpa/")
+        ("org-cn"   . "https://elpa.emacs-china.org/org/")
+        ("gun-cn"   . "https://elpa.emacs-china.org/gnu/")))
+
+(provide 'init-archives)
