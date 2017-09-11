a875d5849a3edc05056d21ddda45583ce61e13d1
Ging.wu <wu.tongqing@gmail.com>
2017-09-11 11:56:46 +0800

    modify my dot file

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,5 @@ if not test -f ~/.config/fish/functions/fisher.fish
 end
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+set -g fish_user_paths "/usr/local/opt/imagemagick@6/bin" $fish_user_paths
+set -g fish_user_paths "$JAVA_HOME/bin" $fish_user_paths
---
+++ fish/functions/__can_run_command.fish
@@ -0,0 +1,1 @@
+/Users/ging/.config/fisherman/nvm/__can_run_command.fish
