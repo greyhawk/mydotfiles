ddf9d983347bcf970798cd891332f660de0e71b2
Ging.wu <wu.tongqing@gmail.com>
2017-11-01 23:30:06 +0800

    modify fish file

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,7 @@ if not test -f ~/.config/fish/functions/fisher.fish
 end
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+set -e fish_user_paths
 set -U fish_user_paths "/usr/local/opt/imagemagick@6/bin" $fish_user_paths
 set -U fish_user_paths "$JAVA_HOME/bin" $fish_user_paths
 set -U fish_user_paths "/Users/ging/.nvm/versions/node/v6.9.5/bin" $fish_user_paths
@@ -13,5 +14,4 @@ set -U fish_user_paths /usr/local/opt/openssl/bin $fish_user_paths
 set -U fish_user_paths /usr/local/opt/go/libexec/bin /Users/ging/Codes/golang/bin $fish_user_paths
 set -U fish_user_paths /opt/X11/bin /Library/TeX/texbin $fish_user_paths
 set -U fish_user_paths /Applications/Wireshark.app/Contents/MacOS $fish_user_paths
-
 
