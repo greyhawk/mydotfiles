99cee34a965171c6afd3cd96a5ee098965ef90a8
Ging.wu <wu.tongqing@gmail.com>
2019-03-18 21:29:38 +0800

    备份

--- fish/config.fish
+++ fish/config.fish
@@ -13,7 +13,6 @@ set -g fish_user_paths "$JAVA_HOME/bin" $fish_user_paths
 set -g fish_user_paths "/Users/ging/.nvm/versions/node/v8.11.1/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/Cellar/pyenv/1.2.8/libexec" $fish_user_paths
 set -g fish_user_paths "/Users/ging/.pyenv/shims" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/openssl/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/go/libexec/bin" $fish_user_paths
 set -g fish_user_paths "/Users/ging/Codes/golang/bin" $fish_user_paths
 set -g fish_user_paths "/opt/X11/bin" $fish_user_paths
@@ -21,7 +20,14 @@ set -g fish_user_paths "/Library/TeX/texbin" $fish_user_paths
 set -g fish_user_paths "/Applications/Wireshark.app/Contents/MacOS" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/texinfo/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/gettext/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/openssl/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/llvm/bin" $fish_user_paths
 set -g fish_user_paths "/Users/ging/.local/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/krb5/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/krb5/sbin" $fish_user_paths
+
 
 set -gx LANG zh_CN.UTF-8
 set -gx LC_CTYPE zh_CN.UTF-8
+set -gx LDFLAGS "-L/usr/local/opt/llvm/lib"
+set -gx CPPFLAGS "-I/usr/local/opt/llvm/include"
