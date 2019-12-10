7a22da06c9c93626d09b0d9cc13a49b04883b4d9
Ging.wu <wu.tongqing@gmail.com>
2019-12-10 11:03:16 +0800

    提交 config fish

--- fish/config.fish
+++ fish/config.fish
@@ -12,10 +12,10 @@ set -g fish_user_paths "/Applications/Wireshark.app/Contents/MacOS" $fish_user_p
 set -g fish_user_paths "/opt/X11/bin" $fish_user_paths
 set -g fish_user_paths "$JAVA_HOME/bin" $fish_user_paths
 set -g fish_user_paths "/Library/TeX/texbin" $fish_user_paths
+set -g fish_user_paths "/Users/ging/.local/bin" $fish_user_paths
 set -g fish_user_paths "/Users/ging/.nvm/versions/node/v8.11.1/bin" $fish_user_paths
 set -g fish_user_paths "/Users/ging/.pyenv/shims" $fish_user_paths
 set -g fish_user_paths "/Users/ging/Codes/golang/bin" $fish_user_paths
-set -g fish_user_paths "/Users/ging/.local/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/pyenv/libexec" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/go/libexec/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/imagemagick@6/bin" $fish_user_paths
@@ -25,6 +25,7 @@ set -g fish_user_paths "/usr/local/opt/openssl/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/llvm/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/krb5/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/krb5/sbin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/binutils/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 
 
