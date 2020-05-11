ff8514da0cd086ae89d25d67f6ddd3fd4ac4f02c
Ging.wu <wu.tongqing@gmail.com>
2020-05-11 23:22:33 +0800

    配置 spacemacs

--- spacemacs/user-init-archives.el
+++ spacemacs/user-init-archives.el
@@ -1,8 +1,8 @@
-;; (setq configuration-layer--elpa-archives
+;; (setq configuration-layer-elpa-archives
 ;;       '(("melpa-cn" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
 ;;         ("org-cn"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")
 ;;         ("gnu-cn"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")))
-(setq configuration-layer--elpa-archives
+(setq configuration-layer-elpa-archives
       '(("melpa-cn" . "http://elpa.emacs-china.org/melpa/")
         ("org-cn"   . "http://elpa.emacs-china.org/org/")
         ("gnu-cn"   . "http://elpa.emacs-china.org/gnu/")))
--- spacemacs/user-init-scheme.el
+++
@@ -1,4 +0,0 @@
-(setq geiser-active-implementations '(racket))
-(setq geiser-default-implementation 'racket)
-
-(provide 'user-init-scheme)
